3634b16074461d589a6b725a7f43000f4417afee
HILLELWIE_DAHLIN <HILLEWIE.DAHLIN@LIVE.SE>
2020-11-30 11:26:57 +0100

    kod klar med css

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@
  *  Generate an API key here: http://www.omdbapi.com/apikey.aspx
  */
 
-
 /**
 * According to documentation, you need at least 2 parameters when calling the API http://www.omdbapi.com/
 * 1 Required parameter: apikey
@@ -21,46 +20,60 @@
 * http://www.omdbapi.com/?apikey=[yourkey]&s=star trek&type=2020
 *
 */
-let url = 'http://www.omdbapi.com/?apikey=df23ef2&s=star trek';
+function getanswer(){
+  let url = 'http://www.omdbapi.com/?apikey=df23ef2';
+  let userInput = document.getElementById("userinput").value;
+  let type = document.getElementById("select-type").value;
+  //let year = document.getElementById("select-year").value;
+  url += '&s=' + userInput;
+  
+   if (type.trim() != "Type") {
+     url += '&type=' + type;
+  } 
+ 
+  //  if (year.trim() !="Release year") {
+  //    url += '&y=' + year;
+  //  }
+   console.log(url)
+ 
+   fetch(url)
+   .then((response) => {
+       if (!response.ok) {
+           throw new Error('Somethings wrong with your request');
+       }
+       return response.text();
+   })
+   .then((data) => {
+     var output = JSON.parse(data);
+ 
+     for (var movie of output['Search']) {
+       getMovie(movie)
+       console.log(movie)
+     }
+   
+   })
+   .catch((error) => {
+       console.log(error);
+   })
+ 
+ 
+ function getMovie(input) {
+   let showResult = document.getElementById("result");  // html-display
+   let htmlResult = 
+   `<div class="row">
+      <div class="col-md-3">
+        <img src="${input.Poster}">
+        <h2>${input.Title}</h2>
+          <ul class="list-group">
+            <li class="list-group-item"><strong>Year released:</strong>${input.Year}</li>
+            <li class="list-group-item"><strong>Type:</strong>${input.Type}</li>
+            <li class="list-group-item"><strong>Imdb ID:</strong>${input.imdbID}</li>
+            <li class="list-group-item"><strong>IMDB page:</strong><a href='https://www.imdb.com/title/${input.imdbID}/' target='_blank'> Home page</a></li>
+          </ul>
+      </div>
+  </div>`;
 
-let userInput = document.getElementById("userinput");
-let type = document.getElementById("select-type");
-let year = document.getElementById("select-year");
-let searchBtn = document.getElementById("search-btn");
-let showResult = document.getElementById("movie-result");
-
-var data;
-
-
-function getanswer(q){
-$.get("https://www.omdbapi.com/?s="+q+"&apikey=ba1f4581", function(rawdata){
-var rawstring =JSON.stringify(rawdata);
-data =JSON.parse(rawstring);
-var title = data.Search[0].Title;
-var year = data.Search[0].Year; 
-var type = data.Search[0].Type; 
-var id = data.Search[0].imdbID;
-var totalMovies = data.Search[0].totaltResults;
-var imdburl="https://www.imdb.com/title/"+data.Search[0].imdbID+"/";
-
-var posterurl =data.Search[0].Poster;
-document.getElementById('answer').innerHTML=
-   // "<h1>"+title+"</h1><br> <img src= '"+posterurl+"'><br><p> Year Released:"+year+"</p> <br> <p> IMDB page: <a href='"+imdburl+"'target='_blank'>"+imdburl+"</a></p>"; }); }
-
-        `<div class="row">
-          <div class="col-md-3">
-            <img src="${posterurl}" class="thumbnail">
-            <h2>${title}</h2>
-            <ul class="list-group">
-              <li class="list-group-item"><strong>Year released:</strong>${year}</li>
-              <li class="list-group-item"><strong>Type:</strong>${type}</li>
-              <li class="list-group-item"><strong>Imdb ID:</strong>${id}</li>
-              <li class="list-group-item"><strong>IMDB page:</strong><a href='${imdburl}'target='_blank'> Home page</a></li>
-            </ul>
-          </div>
-        </div>`; 
-    }); 
-    // for(let movie of totalMovies){
-    //     return
-    // }
-}
+   showResult.innerHTML += htmlResult;
+ }
+  //Töm diven efter varje nytt sök!
+ }
